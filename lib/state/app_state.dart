32a9f1dac8711696688f506a8cd4fd70d0c3f284
dcbc007 <61343313+dcbc007@users.noreply.github.com>
2026-09-25 01:26:30 +0800

    Show 1.1.2 fallback app version

--- lib/state/app_state.dart
+++ lib/state/app_state.dart
@@ -23,7 +23,7 @@ class AppState extends ChangeNotifier {
   bool connecting = false;
   bool coreReady = false;
   String coreVersion = '';
-  String appVersion = '1.1.1';
+  String appVersion = '1.1.2';
   String mode = '智能模式';
   String downloadSpeed = '0 B/s';
   String uploadSpeed = '0 B/s';
